cbedd66b00dece7b219f2194806ad143e157324e
Ju Hyeon Park <wheredidjupark@gmail.com>
2016-01-09 21:41:08 -0800

    added geolocation and links for all anchors

--- main.js
+++ main.js
@@ -7,4 +7,24 @@ $(document).ready(function(){
 	$(".delete-button").on("click", function(){
 		$(".hamburger-menu").toggleClass("hidden");
 	});
+
+	$(".use-my-location").on("click", function(){
+
+		if ("geolocation" in navigator) {
+
+  		/* geolocation is available */
+  			navigator.geolocation.getCurrentPosition(function(position) {
+  				console.log(position.coords.latitude, position.coords.longitude);
+			});
+		}
+
+	});
+
+	$("#myCarousel").swiperight(function() {
+      $(this).carousel('prev');
+    });
+
+   $("#myCarousel").swipeleft(function() {
+      $(this).carousel('next');
+   });
 });
